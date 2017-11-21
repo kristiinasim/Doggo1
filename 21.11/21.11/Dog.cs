b5b07cf4ed264f6d1af1caabe0b5602c25a8c815
Toomas Luts <toomasluts1@gmail.com>
2017-11-21 09:15:53 +0200

    objects

---
+++ 21.11/21.11/Dog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektid
+{
+    class Dog : Mammal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+        public int RealYears { get; set; }
+        public Dog()
+        {
+            Name = "Rex";
+            Age = 2;
+            Color = "Brown";
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Auh auh!!!");
+        }
+
+        public int CalculateDogYears()
+        {
+            RealYears = Age * 7;
+            return RealYears;
+        }
+    }
+}
---
+++ 21.11/21.11/Mammal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objektid
+{
+    class Mammal
+    {
+        public Mammal()
+        {
+            Numberofeyes = 2;
+            Iscarnivore = true;
+            Breathe();
+        }
+
+        protected int Age { get; set; }
+        public int Numberofeyes { get; set; }
+        public bool Iscarnivore { get; set; }
+        public void Breathe()
+        {
+            Console.WriteLine("I am breathing");
+
+        }
+        public void Setage(int age)
+        {
+            Age = age;
+        }
+    }
+}
